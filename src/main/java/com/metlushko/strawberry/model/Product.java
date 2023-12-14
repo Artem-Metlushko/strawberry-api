d6354fa1d05a1d5b94d4713c4820469357c00947
“Artem <3947710@gmail.com>
2023-12-14 10:25:40 +0300

    add:feature/1 model

---
+++ src/main/java/com/metlushko/strawberry/model/Product.java
@@ -0,0 +1,23 @@
+package com.metlushko.strawberry.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Product {
+
+    private UUID id;
+    private String section;
+    private String brand;
+    private String model;
+    private List<Feedback> feedbacks;
+
+}
