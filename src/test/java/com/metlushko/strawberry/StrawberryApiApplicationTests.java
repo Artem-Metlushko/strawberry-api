9fdd370048a36694ede59eb8c7729171c053828f
“Artem <3947710@gmail.com>
2023-12-14 09:27:52 +0300

    init

---
+++ src/test/java/com/metlushko/strawberry/StrawberryApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.metlushko.strawberry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StrawberryApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
